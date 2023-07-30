0f1861f642482ab85bdb825f7acbbc578c924b3f
Bryson Tang <brysontang@gmail.com>
2023-07-30 11:14:59 -0400

    Uses the p5js-renderer-svelte package successfully

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	// @ts-ignore
+	import { P5Renderer } from 'p5js-renderer-svelte';
+
+	let sketch = `
+    function setup() {
+      createCanvas(400, 400);
+    }
+
+    function draw() {
+      background(220);
+      ellipse(mouseX, mouseY, 80, 80);
+    }
+  `;
+
+</script>
+
+Boi
+<P5Renderer {sketch} />
+
+<style>
+	/* CSS styles go here */
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+</style>
