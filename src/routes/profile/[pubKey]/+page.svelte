53b878b35ac0b8f37af9efdff252be3b4aa6e833
Bryson Tang <brysontang@gmail.com>
2023-09-26 22:49:01 -0400

    + Page to show user posts

--- src/routes/profile/[pubKey]/+page.svelte
+++ src/routes/profile/[pubKey]/+page.svelte
@@ -1,11 +1,38 @@
-<script>
+<script lang="ts">
+	// @ts-ignore
+	import FeedItem from '$lib/FeedItem/FeedItem.svelte';
+
+	import { onMount } from 'svelte';
+	import { getNostrPosts } from '$lib/api/nostr';
+
 	export let data;
+
+	let feed: any = [];
+
+	onMount(async () => {
+		feed = await getNostrPosts({ authors: [data['pubKey']] });
+	});
 </script>
 
-<div>{data['pubKey']}</div>
+<div id="feed-page-content">
+	{#each feed as event}
+		<div>
+			<FeedItem {event} />
+		</div>
+	{/each}
+</div>
 
 <style>
-	div {
+	/* CSS styles go here */
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+		color: 'white';
+	}
+
+	#feed-page-content {
 		color: white;
 	}
 </style>
