3666970f8bc24973a0dae40e23c80fac426337c2
Bryson Tang <brysontang@gmail.com>
2023-10-07 14:19:28 -0400

    * Edit profile form now flex column

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -15,10 +15,21 @@
 </script>
 
 <div>
-	<form>
+	<form class="form-container">
 		<label for="username">Title</label>
 		<input type="text" id="username" name="username" placeholder="username" bind:value={username} />
 		<input type="text" id="avatar" name="avatar" placeholder="avatar" bind:value={avatar} />
 		<button type="submit" on:click={updateUserName}>Submit</button>
 	</form>
 </div>
+
+<style>
+	.form-container {
+		padding: 8px;
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
+		justify-content: center;
+		width: 35%;
+	}
+</style>
